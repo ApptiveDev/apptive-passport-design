f8c4bfb604bc92c84cab1cefc434837ffb9461e3
cla6shade <cla6shade@gmail.com>
2024-09-18 18:06:58 +0900

    chore: vite config 설정
    dtsPlugin이 아닌 tsc에서 타입을 생성하도록 변경, path alias 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import dtsPlugin from 'vite-plugin-dts';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   build: {
@@ -22,8 +23,10 @@ export default defineConfig({
       }
     }
   },
-  plugins: [dtsPlugin({
-    rollupTypes: true,
-    tsconfigPath: './tsconfig.app.json'
-  })]
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+    }),
+    tsconfigPaths()
+  ],
 })
