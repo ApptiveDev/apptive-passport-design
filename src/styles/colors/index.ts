8b5e1d701551282985d3034a204a9e715534d1df
cla6shade <cla6shade@gmail.com>
2024-09-18 20:11:32 +0900

    feat: color module

---
+++ src/styles/colors/index.ts
@@ -0,0 +1,82 @@
+const lightColors = {
+  primary: {
+    main: '#FF9433',
+    lighten: '#FFA14D',
+    darken: '#FF861A',
+    passive: 'rgba(255, 148, 51, 0.13)',
+  },
+  text: {
+    prominent: '#191B1C',
+    moderate: '#71787F',
+    subtle: '#B2B6BB',
+  },
+  background: {
+    main: '#FFFFFF',
+    lighten: '#FFFFFF',
+    darken: '#F2F3F4',
+  },
+  border: {
+    subtle: '#ECEDEE',
+    prominent: '#DFE1E3',
+  },
+  absolute: {
+    black: '#191B1C',
+    white: '#FFFFFF',
+  },
+  other: {
+    link: '#3C89FF',
+    success: '#45C768',
+    warn: '#FFBF44',
+    error: '#FC4F4F',
+  },
+  brand: {
+    background: '#131415',
+    text: '#FFFFFF',
+    primary: '#FF7900',
+  },
+};
+
+const darkColors = {
+  primary: {
+    main: '#FFA14D',
+    lighten: '#E99346',
+    darken: '#FFAF66',
+    passive: 'rgba(255, 161, 77, 0.13)',
+  },
+  text: {
+    prominent: '#E5E7E8',
+    moderate: '#7E858D',
+    subtle: '#585D63',
+  },
+  background: {
+    main: '#131415',
+    lighten: '#191B1C',
+    darken: '#0D0D0E',
+  },
+  border: {
+    subtle: '#26282B',
+    prominent: '#202224',
+  },
+  absolute: {
+    black: '#131415',
+    white: '#E5E7E8',
+  },
+  other: {
+    link: '#1C6AE0',
+    success: '#14B03E',
+    warn: '#EE9D00',
+    error: '#D02727',
+  },
+  brand: {
+    background: '#202224',
+    text: '#E5E7E8',
+    primary: '#FF7900',
+  },
+};
+
+const colors = {
+  light: lightColors,
+  dark: darkColors,
+};
+
+export default colors;
