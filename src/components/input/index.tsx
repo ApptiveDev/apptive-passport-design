ddff6b9efe92ee8dc05907649c919dbddc4b0d38
cla6shade <cla6shade@gmail.com>
2024-09-22 00:09:38 +0900

    feat: input

---
+++ src/components/input/index.tsx
@@ -0,0 +1,89 @@
+import {
+  ChangeEvent, Dispatch,
+  HTMLAttributes,
+  HTMLInputTypeAttribute, SetStateAction, useRef, useState,
+} from 'react';
+import { CSSObject } from '@emotion/react';
+import {
+  iconStyle,
+  inputContainerStyle,
+  inputStyle, labelStyle,
+} from '@components/input/styles';
+import toggleShowIcon from '@assets/icons/eye.svg';
+import toggleHideIcon from '@assets/icons/eye-off.svg';
+import { generateRandomId } from '@/utils';
+
+interface PasswordInputProps extends HTMLAttributes<HTMLInputElement> {
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  iconSrc?: string;
+  enableToggleShow?: boolean;
+  type: HTMLInputTypeAttribute;
+  label?: string;
+  css?: CSSObject;
+}
+
+function Input({
+  value, onChange, iconSrc, enableToggleShow, type, label, css, ...rest
+}: PasswordInputProps) {
+  const inputId = useRef(generateRandomId());
+  const [isHidden, setIsHidden] = useState(true);
+
+  if (enableToggleShow && type !== 'password') {
+    throw new Error('Cannot enable toggle while the type of input is not password');
+  }
+
+  return (
+    <>
+      {
+        label
+          ? <label htmlFor={inputId.current} css={labelStyle}>{label}</label>
+          : null
+      }
+      <div css={inputContainerStyle}>
+        {
+          iconSrc
+            ? <img src={iconSrc} alt="input icon" css={iconStyle()} />
+            : null
+        }
+        <input
+          id={inputId.current}
+          value={value}
+          onChange={onChange}
+          css={[inputStyle(iconSrc, enableToggleShow), css]}
+          type={type === 'password' && isHidden
+            ? 'password'
+            : 'text'}
+          {...rest}
+        />
+        {
+        enableToggleShow
+          ? <ToggleVisibilityIcon isHidden={isHidden} setIsHidden={setIsHidden} />
+          : null
+      }
+      </div>
+    </>
+  );
+}
+
+interface ToggleVisibilityIconProps {
+  isHidden: boolean;
+  setIsHidden: Dispatch<SetStateAction<boolean>>
+}
+
+function ToggleVisibilityIcon({
+  isHidden,
+  setIsHidden,
+}: ToggleVisibilityIconProps) {
+  return (
+    <img
+      src={isHidden ? toggleShowIcon : toggleHideIcon}
+      alt="toggle show"
+      css={iconStyle(true)}
+      onClick={() => setIsHidden((prevState: boolean) => !prevState)}
+      role="presentation"
+    />
+  );
+}
+
+export default Input;
---
+++ src/components/input/styles.ts
@@ -0,0 +1,52 @@
+import { css, CSSObject } from '@emotion/react';
+import corner from '@styles/corner';
+import colors from '@styles/colors';
+
+function inputStyle(iconSrc?: string, enableToggleShow?: boolean): CSSObject {
+  return (
+    css`
+      padding: 10px ${enableToggleShow ? '34px' : '10px'} 10px ${iconSrc ? '34px' : '10px'};
+      border-radius: ${corner.small};
+      border: 1px solid ${colors.light.border.subtle};
+      width: 100%;
+      box-sizing: border-box;
+      font-size: 15px;
+      
+      &:focus {
+        outline: none;
+        border: 1px solid ${colors.light.border.prominent}
+      }
+    `
+  );
+}
+
+function iconStyle(isToggleShowIcon?: boolean) {
+  return (
+    css`
+      position: absolute;
+      left: ${isToggleShowIcon ? 'auto' : '10px'};
+      right: ${isToggleShowIcon ? '10px' : 'auto'};
+      top: 10px;
+      width: 16px;
+      height: 16px;
+      cursor: ${isToggleShowIcon ? 'pointer' : 'default'};
+    `
+  );
+}
+
+const inputContainerStyle = css`
+  position: relative;
+`;
+
+const labelStyle = css`
+  margin: 0;
+  font-size: 13px;
+  color: ${colors.light.text.moderate};
+`;
+
+export {
+  iconStyle,
+  inputStyle,
+  labelStyle,
+  inputContainerStyle,
+};
