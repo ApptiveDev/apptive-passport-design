ac6c36ff4072df82d666a4b385641a7cfde04354
cla6shade <cla6shade@github.com>
2024-09-28 01:43:07 +0900

    refactor: custom hook으로 스타일을 불러올 수 있도록 변경

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,7 +1,7 @@
-import { buttonIconStyle, buttonStyle } from '@components/button/styles';
 import { ButtonHTMLAttributes, forwardRef, ReactNode } from 'react';
 import { CSSObject } from '@emotion/react';
 import DynamicIcon from '@components/internal/dynamic-icon';
+import useButtonStyle from '@components/button/useButtonStyle';
 import { ButtonTheme } from '@/types';
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
@@ -12,16 +12,20 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
   children, theme = 'default', css, icon, ...rest
-}: ButtonProps, ref) => (
-  <button
-    css={[buttonStyle(theme), css]}
-    type="button"
-    ref={ref}
-    {...rest}
-  >
-    <DynamicIcon icon={icon} css={buttonIconStyle} />
-    {children}
-  </button>
-));
+}: ButtonProps, ref) => {
+  const { buttonStyle, buttonIconStyle } = useButtonStyle({ buttonTheme: theme });
+
+  return (
+    <button
+      css={[buttonStyle, css]}
+      type="button"
+      ref={ref}
+      {...rest}
+    >
+      <DynamicIcon icon={icon} css={buttonIconStyle} />
+      {children}
+    </button>
+  );
+});
 
 export default Button;
