61a5deadc5a5c2cc46d870187c89efbe063a48fd
cla6shade <cla6shade@github.com>
2024-09-27 17:42:41 +0900

    refactor: 버튼 테마명에 대해 변수명 변경

--- src/components/button/styles.ts
+++ src/components/button/styles.ts
@@ -2,7 +2,7 @@ import colorTheme from '@styles/colors';
 import { css } from '@emotion/react';
 import { ButtonTheme } from '@/types';
 
-function buttonStyle(theme: ButtonTheme) {
+function buttonStyle(buttonTheme: ButtonTheme) {
   return (
     css`
       display: flex;
@@ -11,17 +11,17 @@ function buttonStyle(theme: ButtonTheme) {
       outline: none;
       padding: 10px 18px;
       border-radius: 100px;
-      color: ${theme === 'dark' ? colorTheme.primary.main : colorTheme.text.prominent};
-      border: ${getBorderStyle(theme)};
-      background-color: ${getBackgroundColor(theme)};
+      color: ${buttonTheme === 'dark' ? colorTheme.primary.main : colorTheme.text.prominent};
+      border: ${getBorderStyle(buttonTheme)};
+      background-color: ${getBackgroundColor(buttonTheme)};
       transition: background-color 0.3s ease, color 0.3s ease, border-color 0.3s ease;
       cursor: pointer;
       gap: 5px;
 
       &:hover {
-        background-color: ${getHoverBackgroundColor(theme)};
-        color: ${getHoverColor(theme)};
-        border-color: ${getHoverBorderColor(theme)};
+        background-color: ${getHoverBackgroundColor(buttonTheme)};
+        color: ${getHoverColor(buttonTheme)};
+        border-color: ${getHoverBorderColor(buttonTheme)};
       }
   `);
 }
@@ -31,54 +31,54 @@ const buttonIconStyle = css`
   height: 16px;
 `;
 
-function getBackgroundColor(theme: ButtonTheme) {
-  if (theme === 'light-outlined') {
+function getBackgroundColor(buttonTheme: ButtonTheme) {
+  if (buttonTheme === 'light-outlined') {
     return 'transparent';
   }
 
-  if (theme === 'dark') {
+  if (buttonTheme === 'dark') {
     return colorTheme.text.prominent;
   }
 
   return colorTheme.background.main;
 }
 
-function getBorderStyle(theme: ButtonTheme) {
-  if (theme === 'light-outlined') {
+function getBorderStyle(buttonTheme: ButtonTheme) {
+  if (buttonTheme === 'light-outlined') {
     return `2px solid ${colorTheme.absolute.black}`;
   }
 
   const baseStyle = '1px solid ';
 
-  return baseStyle + (theme === 'dark' ? 'transparent' : colorTheme.text.subtle);
+  return baseStyle + (buttonTheme === 'dark' ? 'transparent' : colorTheme.text.subtle);
 }
 
-function getHoverBackgroundColor(theme: ButtonTheme) {
-  if (theme === 'light-outlined') {
+function getHoverBackgroundColor(buttonTheme: ButtonTheme) {
+  if (buttonTheme === 'light-outlined') {
     return colorTheme.text.prominent;
   }
 
-  if (theme === 'dark') {
+  if (buttonTheme === 'dark') {
     return colorTheme.primary.main;
   }
 
   return colorTheme.background.darken;
 }
 
-function getHoverColor(theme: ButtonTheme) {
-  if (theme === 'light-outlined') {
+function getHoverColor(buttonTheme: ButtonTheme) {
+  if (buttonTheme === 'light-outlined') {
     return colorTheme.background.main;
   }
 
   return colorTheme.text.prominent;
 }
 
-function getHoverBorderColor(theme: ButtonTheme) {
-  if (theme === 'light-outlined') {
+function getHoverBorderColor(buttonTheme: ButtonTheme) {
+  if (buttonTheme === 'light-outlined') {
     return colorTheme.absolute.black;
   }
 
-  return theme === 'dark' ? colorTheme.primary.main : colorTheme.border.prominent;
+  return buttonTheme === 'dark' ? colorTheme.primary.main : colorTheme.border.prominent;
 }
 
 export {
