003f5fa569ad97e1381071a5b0cd2a4181fb6a1e
cla6shade <cla6shade@gmail.com>
2024-09-28 14:30:28 +0900

    refactor: label 관련 변경사항 반영

--- src/components/textarea/index.tsx
+++ src/components/textarea/index.tsx
@@ -5,6 +5,7 @@ import {
 } from 'react';
 import { CSSObject } from '@emotion/react';
 import useTextAreaStyle from '@components/textarea/useTextAreaStyle';
+import Label from '@components/label';
 import { generateRandomId } from '@/utils';
 
 interface TextareaProps extends HTMLAttributes<HTMLTextAreaElement> {
@@ -19,16 +20,16 @@ const TextArea = forwardRef<HTMLTextAreaElement, TextareaProps>(({
   onChange, label, css, rows = 4, cols = 50, maxLength, ...rest
 }, ref) => {
   const textareaId = useRef(generateRandomId());
-  const { labelStyle, textAreaStyle } = useTextAreaStyle();
+  const { textAreaStyle } = useTextAreaStyle();
 
   return (
     <>
       {
         label
           ? (
-            <label htmlFor={textareaId.current} css={labelStyle}>
+            <Label htmlFor={textareaId.current}>
               <p>{label}</p>
-            </label>
+            </Label>
           )
           : null
       }
