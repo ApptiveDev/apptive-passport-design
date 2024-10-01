8c1d2f10c76eaf3369a9422a10219cc0d086fd6b
cla6shade <cla6shade@github.com>
2024-10-01 19:52:42 +0900

    feat: 컴포넌트 export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import Button from '@components/button';
+import Checkbox from '@components/checkbox';
+import Radio from '@components/radio';
+import Input from '@components/input';
+import Select from '@components/select';
+import Switch from '@components/switch';
+import Tag from '@components/tag';
+import TextArea from '@components/textarea';
+import Label from '@components/label';
 
 export {
-  // eslint-disable-next-line import/prefer-default-export
   Button,
+  Checkbox,
+  Radio,
+  Input,
+  Select,
+  Switch,
+  Tag,
+  TextArea,
+  Label,
 };
